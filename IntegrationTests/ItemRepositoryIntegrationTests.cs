8e116e68f7d316c682265f93ad12b60f20be17dc
Noah Shoap <nshoap@vols.utk.edu>
2024-02-10 17:57:48 -0500

    feat(TestHelpers): ItemHelper

--- IntegrationTests/ItemRepositoryIntegrationTests.cs
+++ IntegrationTests/ItemRepositoryIntegrationTests.cs
@@ -1,9 +1,28 @@
+using Backend1.Data;
+using Backend1.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace IntegrationTests;
 
 public class ItemRepositoryIntegrationTests
 {
-    [Fact]
-    public void Test1()
+    private readonly DbContextOptions<InventoryContext> _options;
+
+    public ItemRepositoryIntegrationTests()
     {
+        _options = new DbContextOptionsBuilder<InventoryContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+    }
+    
+    [Fact]
+    public void TestAddItem()
+    {
+        using (var db = new InventoryContext(_options))
+        {
+            var sut = new ItemRepository(db);
+            
+            
+        }
     }
 }
---
+++ TestHelpers/ItemHelper.cs
@@ -0,0 +1,17 @@
+﻿using Backend1.Models;
+
+namespace TestHelpers;
+
+public static class ItemHelper
+{
+    public static Item GetBoilerplateItem()
+    {
+        return new Item
+        {
+            Id = 0,
+            BusinessId = 0,
+            Name = "FakeItem",
+            Quantity = 5
+        };
+    }
+}
