6c05e407c879c4928e19e78da7eb359c273742de
Noah Shoap <nshoap@vols.utk.edu>
2024-03-28 21:08:18 -0400

    test(InventoryControllerIntegration): removed most tests

--- IntegrationTests/InventoryControllerIntegrationTests.cs
+++ IntegrationTests/InventoryControllerIntegrationTests.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using Backend1.Models;
-using Newtonsoft.Json;
 using TestHelpers;
 using Xunit.Abstractions;
 
@@ -15,69 +13,6 @@ public class InventoryControllerIntegrationTests : IClassFixture<WebApplicationF
     {
         _factory = factory;
         _output = outputHelper;
-    }
-
-    [Fact (Skip = "Not working with new changes.")]
-    /*
-     * This is one big test for add, update, and delete.
-     * The reason is because these tests deal with the actual inventory database.
-     * I do not feel like making a separate database to test with, so this is my solution.
-     */
-    public async Task AddUpdateDeleteTest()
-    {
-        var item = new Item
-        {
-            Name = "InappropriateTestItem",
-            BusinessId = 42069,
-            Id = 80085,
-            Quantity = 1,
-            Price = 5,
-            About = "This is a test item.",
-            LowStockNotification = 0,
-            Sales = 0
-        };
-
-        var client = await AuthHelper.ConstructAuthorizedClient(_factory);
-
-        // Create
-        var json = JsonConvert.SerializeObject(item);
-        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-        var response = await client.PostAsync("api/inventory/add", content);
-
-        response.EnsureSuccessStatusCode();
-        
-        var item2 = new Item
-        {
-            Name = "InappropriateTestItem",
-            BusinessId = 42069,
-            Id = 80085,
-            Quantity = 10,
-            Price = 7,
-            About = "This is a test item.",
-            LowStockNotification = 0,
-            Sales = 5
-        };
-    
-        // Update
-        json = JsonConvert.SerializeObject(item2);
-        content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-        response = await client.PutAsync("api/inventory/update", content);
-
-        response.EnsureSuccessStatusCode();
-
-        // Delete
-        response = await client.DeleteAsync($"api/inventory/remove/{item2.Id}");
-        response.EnsureSuccessStatusCode();
-    }
-    
-    [Fact (Skip = "Not working with new changes")]
-    public async Task GetInventoryTest()
-    {
-        const int hardCodedBusinessId = 10;
-        var client = await AuthHelper.ConstructAuthorizedClient(_factory);
-
-        var response = await client.GetAsync($"api/inventory/getInventory/{hardCodedBusinessId}");
-        response.EnsureSuccessStatusCode();
     }
 
     [Fact]
